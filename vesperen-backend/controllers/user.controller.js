d6c4fa714f7cb1e5dd4e1b95046292f8e91b508c
Célia S <celoo.sa@icloud.com>
2025-09-02 20:14:32 +0200

    correction coquille deleted(au lieu de delete)

--- vesperen-backend/controllers/user.controller.js
+++ vesperen-backend/controllers/user.controller.js
@@ -125,7 +125,7 @@ const deleteUser = async (req, res, next) => {
     const deletedUser = await User.findByIdAndDelete(req.params.id);
     return res.status(200).json({
       message: "User deleted successfully",
-      deleteUser,
+      deletedUser,
     });
   } catch (error) {
     return res.status(500).json(error.message);
